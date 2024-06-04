e325c5da55470211a922e23111041910139ae7f2
subaiqiao <1294749811@qq.com>
2024-06-05 01:12:43 +0800

    主题色配置

--- react-bill-test/src/pages/Layout/index.jsx
+++ react-bill-test/src/pages/Layout/index.jsx
@@ -1,10 +1,15 @@
 import {Outlet} from "react-router-dom";
-
+import { Button } from 'antd-mobile'
 const Layout = () => {
     return (
         <div>
             我是layout
             <Outlet/>
+            {/*测试全局生效样式*/}
+            <Button color="primary">测试全局</Button>
+            <div className="purple-theme">
+                <Button color="primary">测试局部</Button>
+            </div>
         </div>
     )
 }
