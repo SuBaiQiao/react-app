10907516940fa074c632a966c2269367f5442311
曹朝禹 <1294749811@qq.com>
2024-06-05 10:08:31 +0800

    月度账单

--- react-bill-test/src/pages/Month/index.jsx
+++ react-bill-test/src/pages/Month/index.jsx
@@ -1,7 +1,79 @@
+import {NavBar, DatePicker} from 'antd-mobile'
+import './index.scss'
+import {useMemo, useState} from "react";
+import classNames from "classnames";
+import dayjs from "dayjs";
+import {useSelector} from "react-redux";
+import _ from "lodash";
+
 const Month = () => {
+    const [dateVisible, setDateVisible] = useState(false)
+    const [currentDate, setCurrentDate] = useState(new Date())
+    const billList = useSelector(state => state.bill.billList);
+    const monthGroup = useMemo(() => {
+        return _.groupBy(billList, (item) => dayjs(item.date).format('YYYY-MM'));
+    }, [billList])
+    const [currentMonthList, setCurrentMonthList] = useState([])
+    const onConfirm = (e) => {
+        setCurrentDate(e);
+        setDateVisible(false);
+        const formatDate = dayjs(e).format('YYYY-MM');
+        const monthList = monthGroup[formatDate];
+        setCurrentMonthList(monthList)
+    }
+    const monthResult = useMemo(() => {
+        const pay = currentMonthList.filter(item => item.type === 'pay').reduce((a, c) => a + c.money, 0);
+        const income = currentMonthList.filter(item => item.type === 'income').reduce((a, c) => a + c.money, 0);
+        return {
+            pay,
+            income,
+            total: pay + income
+        }
+    }, [currentMonthList]);
     return (
-        <div>我是month</div>
+        <div className="monthlyBill">
+            <NavBar className="nav" backArrow={false}>
+                月度收支
+            </NavBar>
+            <div className="content">
+                <div className="header">
+                    {/* 时间切换区域 */}
+                    <div className="date" onClick={() => setDateVisible(true)}>
+                        <span className="text">
+                          {dayjs(currentDate).format('YYYY')} | {dayjs(currentDate).format('M')}月账单
+                        </span>
+                        <span className={classNames('arrow', dateVisible && 'expand')}></span>
+                    </div>
+                    {/* 统计区域 */}
+                    <div className='twoLineOverview'>
+                        <div className="item">
+                            <span className="money">{monthResult.pay.toFixed(2)}</span>
+                            <span className="type">支出</span>
+                        </div>
+                        <div className="item">
+                            <span className="money">{monthResult.income.toFixed(2)}</span>
+                            <span className="type">收入</span>
+                        </div>
+                        <div className="item">
+                            <span className="money">{monthResult.total.toFixed(2)}</span>
+                            <span className="type">结余</span>
+                        </div>
+                    </div>
+                    {/* 时间选择器 */}
+                    <DatePicker
+                        className="kaDate"
+                        title="记账日期"
+                        precision="month"
+                        visible={dateVisible}
+                        onCancel={() => setDateVisible(false)}
+                        onConfirm={(e) => onConfirm(e)}
+                        onClose={() => setDateVisible(false)}
+                        max={new Date()}
+                    />
+                </div>
+            </div>
+        </div>
     )
 }
 
-export default Month;
+export default Month
